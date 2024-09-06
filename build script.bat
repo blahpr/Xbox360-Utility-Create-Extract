b4c82b2cfcad0c7b5136cb64c57d609f5ecbbe69
BLAHPR <tendonen7@gmail.com>
2024-09-06 07:16:32 -0700

    added UPX arg to bat file

--- build script.bat
+++ build script.bat
@@ -1,3 +1,3 @@
-pyinstaller --onefile --icon=images/360.ico --add-data "x_tool;x_tool" --add-data "images;images" --add-data "x_ISO;x_ISO" --name "360 Utility Batch Create Extract v1.2" main.pyw
+pyinstaller --onefile --icon=images/360.ico --add-data "x_tool;x_tool" --add-data "images;images" --add-data "x_ISO;x_ISO" --name "360 XISO Batch Create Extract v1.2" --upx-dir "C:\upx-4.2.4-win64" main.pyw
 
 pause
